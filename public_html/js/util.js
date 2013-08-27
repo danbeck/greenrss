d30817860d30119e2180c0b77eff8b575be90d1d
Daniel Beck <d.danielbeck@googlemail.com>
2013-08-27 22:23:19 +0200

    code cleanup.

--- public_html/js/util.js
+++ public_html/js/util.js
@@ -1,94 +1,94 @@
 if (!Function.prototype.bind) {
-	Function.prototype.bind = function(oThis) {
-		if (typeof this !== "function") {
-			// closest thing possible to the ECMAScript 5 internal IsCallable
-			// function
-			throw new TypeError(
-					"Function.prototype.bind - what is trying to be bound is not callable");
-		}
+    Function.prototype.bind = function(oThis) {
+        if (typeof this !== "function") {
+            // closest thing possible to the ECMAScript 5 internal IsCallable
+            // function
+            throw new TypeError(
+                    "Function.prototype.bind - what is trying to be bound is not callable");
+        }
 
-		var aArgs = Array.prototype.slice.call(arguments, 1), fToBind = this, fNOP = function() {
-		}, fBound = function() {
-			return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
-					aArgs.concat(Array.prototype.slice.call(arguments)));
-		};
+        var aArgs = Array.prototype.slice.call(arguments, 1), fToBind = this, fNOP = function() {
+        }, fBound = function() {
+            return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
+                    aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
 
-		fNOP.prototype = this.prototype;
-		fBound.prototype = new fNOP();
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
 
-		return fBound;
-	};
+        return fBound;
+    };
 }
 
 function simpleHttpRequest(url, success, failure) {
-	var request = makeHttpObject();
-	request.open("GET", url, true);
-	request.send(null);
-	request.onreadystatechange = function() {
-		if (request.readyState == 4) {
-			if (request.status == 200)
-				success(request.responseText);
-			else if (failure)
-				failure(request.status, request.statusText);
-		}
-	};
+    var request = makeHttpObject();
+    request.open("GET", url, true);
+    request.send(null);
+    request.onreadystatechange = function() {
+        if (request.readyState === 4) {
+            if (request.status === 200)
+                success(request.responseText);
+            else if (failure)
+                failure(request.status, request.statusText);
+        }
+    };
 }
 
 function $(id) {
-	return document.getElementById(id);
+    return document.getElementById(id);
 }
 
 function isDisplayed(element) {
-	if (!element.style)
-		return true;
-	if (element.style.display !== 'none')
-		return true;
+    if (!element.style)
+        return true;
+    if (element.style.display !== 'none')
+        return true;
 
-	return false;
+    return false;
 }
 
 function show(element) {
-	element.style.display = 'block';
+    element.style.display = 'block';
 }
 
 function hide(element) {
-	element.style.display = 'none';
+    element.style.display = 'none';
 }
 
 function toggle_visibility(e) {
-	if (e.style.display === 'block')
-		e.style.display = 'none';
-	else
-		e.style.display = 'block';
+    if (e.style.display === 'block')
+        e.style.display = 'none';
+    else
+        e.style.display = 'block';
 }
 
 function linkOpenInNewWindow(href, innerHTML) {
-	var a = link(href, innerHTML);
-	a["target"] = "_blank";
-	return a;
+    var a = link(href, innerHTML);
+    a["target"] = "_blank";
+    return a;
 }
 
 function link(href, innerHTML) {
-	var a = document.createElement("a");
-	a["href"] = href;
-	a.innerHTML = innerHTML;
-	return a;
+    var a = document.createElement("a");
+    a["href"] = href;
+    a.innerHTML = innerHTML;
+    return a;
 }
 
 function script(innerHtml) {
-	var script = document.createElement('script');
-	script.innerHTML = innerHtml;
-	return script;
+    var script = document.createElement('script');
+    script.innerHTML = innerHtml;
+    return script;
 }
 
 function createLi(element) {
-	var li = document.createElement("li");
-	li.appendChild(element);
-	return li;
+    var li = document.createElement("li");
+    li.appendChild(element);
+    return li;
 }
 
 function createP(innnerHTML) {
-	var p = document.createElement("p");
-	p.innerHTML = innnerHTML;
-	return p;
+    var p = document.createElement("p");
+    p.innerHTML = innnerHTML;
+    return p;
 }
