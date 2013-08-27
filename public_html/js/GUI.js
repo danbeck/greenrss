e7f9dcb0ca79e4c426c653e8c25f5f2006b445b7
Daniel Beck <d.danielbeck@googlemail.com>
2013-08-27 19:49:24 +0200

    - added night theme - configuration panel now closes as it should - new utilities functions (binds(), makeXmlHttpRequest) - dialog

--- public_html/js/GUI.js
+++ public_html/js/GUI.js
@@ -1,31 +1,32 @@
 function Gui() {
-    this.UI = new UbuntuUI();
-    this.UI.init();
+	var that = this;
 
-    // Set up the app by pushing the main view
-    this.UI.pagestack.push("main-page");
-    var backButton = document.querySelector("li a[data-role=\"back\"]");
+	this.UI = new UbuntuUI();
+	this.UI.init();
 
-    var that = this;
-    this.UI.button('configureButton').click(function() {
-        that.openConfigurePage(this, that.onConfigurationChanged);
-    });
+	// Set up the app by pushing the main view
+	this.UI.pagestack.push("main-page");
+	// var backButton = document.querySelector("li a[data-role=\"back\"]");
 
-    // On clicking the scan button, show the scan page
-    this.UI.button('addFeedButton').click(function() {
-        show("addfeeddialog");
+	this.UI.button('configureButton').click(function() {
+		that.openConfigurePage(this, that.onConfigurationChanged);
+	});
 
-    });
+	// On clicking the scan button, show the scan page
+	this.UI.button('addFeedButton').click(function() {
+		show($("addfeeddialog"));
 
-    this.UI.button('addfeedsuccess').click(function() {
-        var feedSubscriptionURL = $("rssFeed").value;
-        that.onFeedAdded(feedSubscriptionURL);
-        hide("addfeeddialog");
-    });
+	});
 
-    UI.button('addfeedcancel').click(function() {
-        hide("addfeeddialog");
-    });
+	this.UI.button('addfeedsuccess').click(function() {
+		var feedSubscriptionURL = $("rssFeed").value;
+		that.onFeedAdded(feedSubscriptionURL);
+		hide($("addfeeddialog"));
+	});
+
+	UI.button('addfeedcancel').click(function() {
+		hide($("addfeeddialog"));
+	});
 }
 
 Gui.prototype.onConfigurationChanged = function() {
@@ -37,131 +38,170 @@ Gui.prototype.reload = function() {
 };
 Gui.prototype.openConfigurePage = function(openConfigButton) {
 
-    var returnValue = {useTheOldReader: false, useNightMode: false};
+	var that = this;
+	var returnValue = {
+		useTheOldReader : false,
+		useNightMode : false
+	};
 
-    var backButtonEventListener = function() {
+	// var backButtonEventListener = function() {
+	// };
 
-    };
+	var theoldreader_username;
+	var theoldreader_password;
 
-    var theoldreader_username;
-    var theoldreader_password;
+	// this.UI.too
 
-//this.UI.too
+	this.UI.popover(openConfigButton, "configurePopover").toggle();
 
-    this.UI.popover(openConfigButton, "configurePopover").toggle();
+	var configurePopover = $("configurePopover");
+	var leftFloat = parseFloat(configurePopover.style.left);
+	leftFloat = leftFloat - 130;
+	configurePopover.style.left = leftFloat + "px";
 
-    var configurePopover = $("configurePopover");
-    var leftFloat = parseFloat(configurePopover.style.left);
-    leftFloat = leftFloat - 130;
-    configurePopover.style.left = leftFloat + "px";
+	var li = $('extendConfigurationMenuItem');
 
-    var li = $('extendConfigurationMenuItem');
+	li.onclick = function() {
+		that.UI.popover(this, "configurePopover").hide();
 
-    var that = this;
-    li.onclick = function() {
-        that.UI.popover(this, "configurePopover").toggle();
+		that.UI.pagestack.push('extendedConfigurationPage', {
+			subtitle : 'Configuration'
+		});
 
+		hide($("addFeedButton").parentNode);
+		hide($("reloadFeedsButton").parentNode);
+		hide(openConfigButton.parentNode);
 
+		// var useTheOldReader = $("theoldreader_use_sync");
+		//
+		// var theoldreaderUsername = $("theoldreader_username");
+		//
+		// theoldreaderUsername.addEventListener("onkeyup", function() {
+		// theoldreader_username = theoldreaderUsername.value;
+		// });
+		// var theoldreaderPassword = $("theoldreader_password");
+		// theoldreaderUsername.addEventListener("keyup", function() {
+		// theoldreader_password = theoldreaderPassword.value;
+	};
 
-        that.UI.pagestack.push('extendedConfigurationPage',
-                {subtitle: 'Configuration'});
-        var useTheOldReader = $("theoldreader_use_sync");
+	var backButton = document.querySelector("li a[data-role=\"back\"]");
 
-        var theoldreaderUsername = $("theoldreader_username");
+	backButton.onclick = function() {
 
-        theoldreaderUsername.addEventListener("onkeyup", function() {
-            theoldreader_username = theoldreaderUsername.value;
-        });
-        var theoldreaderPassword = $("theoldreader_password");
-        theoldreaderUsername.addEventListener("keyup", function() {
-            theoldreader_password = theoldreaderPassword.value;
-        });
-    };
+		// theoldreaderUsername.addEventListener("onkeyup", function() {
+		// theoldreader_username = theoldreaderUsername.value;
+		// });
+		// theoldreaderUsername.addEventListener("keyup", function() {
+		// theoldreader_password = theoldreaderPassword.value;
+		// });
 
-    return that.onConfigurationChanged(returnValue);
+		that.UI.popover(openConfigButton, "configurePopover").hide();
+		if (isDisplayed($("extendedConfigurationPage")))
+			that.UI.pagestack.pop('extendedConfigurationPage', {
+				subtitle : 'Configuration'
+			});
+
+		show($("addFeedButton").parentNode);
+		show($("reloadFeedsButton").parentNode);
+		show(openConfigButton.parentNode);
+
+		var useTheOldReader = $("theoldreader_use_sync").value;
+		var theoldreaderUsername = $("theoldreader_username").value;
+		var theoldreaderPassword = $("theoldreader_password").value;
+		if (useTheOldReader)
+			returnValue.useTheOldReader = useTheOldReader;
+
+		if (theoldreaderUsername && theoldreaderUsername !== "")
+			returnValue.theoldreaderUsername = theoldreaderUsername;
+
+		if (theoldreaderPassword && theoldreaderPassword !== "")
+			returnValue.theoldreaderPassword = theoldreaderPassword;
+
+		return that.onConfigurationChanged(returnValue);
+
+	};
 };
 
 Gui.prototype.addFeedInGui = function(feedTitle, feedUrl, feedRecord) {
-    var pa = createP(feedTitle);
-    var li = createLi(pa);
-    li["data-rss-link"] = feedUrl;
+	var pa = createP(feedTitle);
+	var li = createLi(pa);
+	li["data-rss-link"] = feedUrl;
 
-    $("feedsAboList").appendChild(li);
+	$("feedsAboList").appendChild(li);
 
-    li.addEventListener('touchstart', function() {
-        li.className += " tapped";
-    });
-    li.addEventListener('touchend', function() {
-        li.className = "";
-    });
+	li.addEventListener('touchstart', function() {
+		li.className += " tapped";
+	});
+	li.addEventListener('touchend', function() {
+		li.className = "";
+	});
 
-    var that = this;
+	var that = this;
 
-    li.onclick = function showFeedEntry() {
-        var clickedFeedUrl = li["data-rss-link"];
+	li.onclick = function showFeedEntry() {
+		var clickedFeedUrl = li["data-rss-link"];
 
-        var feedsAboListElement = $("feedsAboList");
-        var allFeeds = feedsAboListElement.childNodes;
-        for (var i = 0; i < allFeeds.length; i++) {
-            allFeeds[i].removeAttribute("class");
-        }
-        li["className"] = "active";
-        showFeedEntriesInGUI(clickedFeedUrl, feedRecord, that);
-    };
+		var feedsAboListElement = $("feedsAboList");
+		var allFeeds = feedsAboListElement.childNodes;
+		for ( var i = 0; i < allFeeds.length; i++) {
+			allFeeds[i].removeAttribute("class");
+		}
+		li["className"] = "active";
+		showFeedEntriesInGUI(clickedFeedUrl, feedRecord, that);
+	};
 
-    function showFeedEntriesInGUI(clickedFeedUrl, feedRecord, that) {
-        var feedInfo = feedRecord[clickedFeedUrl];
+	function showFeedEntriesInGUI(clickedFeedUrl, feedRecord, that) {
+		var feedInfo = feedRecord[clickedFeedUrl];
 
-        var feedElements = $("feedEntriesList");
-        var fragment = document.createDocumentFragment();
+		var feedElements = $("feedEntriesList");
+		var fragment = document.createDocumentFragment();
 
-        feedElements.innerHTML = '';
-        for (var i = 0; i < feedInfo.entries.length; i++) {
-            var feedEntry = feedInfo.entries[i];
-            addFeedEntriesToFragment(feedEntry, fragment, that);
-        }
-        feedElements.appendChild(fragment);
-    }
+		feedElements.innerHTML = '';
+		for ( var i = 0; i < feedInfo.entries.length; i++) {
+			var feedEntry = feedInfo.entries[i];
+			addFeedEntriesToFragment(feedEntry, fragment, that);
+		}
+		feedElements.appendChild(fragment);
+	}
 
+	function addFeedEntriesToFragment(feedEntry, fragment, that) {
+		var a = document.createElement("a");
+		var p1 = createP(feedEntry.title);
+		var p2 = createP(feedEntry.contentSnippet);
+		p1["data-article"] = feedEntry;
+		a.appendChild(p1);
+		a.appendChild(p2);
+		var li = createLi(a);
 
-    function addFeedEntriesToFragment(feedEntry, fragment, that) {
-        var a = document.createElement("a");
-        var p1 = createP(feedEntry.title);
-        var p2 = createP(feedEntry.contentSnippet);
-        p1["data-article"] = feedEntry;
-        a.appendChild(p1);
-        a.appendChild(p2);
-        var li = createLi(a);
+		fragment.appendChild(li);
 
-        fragment.appendChild(li);
+		li.addEventListener('touchstart', function() {
+			li.className += " tapped";
+		});
+		li.addEventListener('touchend', function() {
+			li.className = "";
+		});
 
-        li.addEventListener('touchstart', function() {
-            li.className += " tapped";
-        });
-        li.addEventListener('touchend', function() {
-            li.className = "";
-        });
+		li.onclick = function() {
+			showArticle(feedEntry, that);
+		};
+	}
 
+	function showArticle(feedEntry, that) {
+		var articleTitle = $("articleTitle");
+		articleTitle.innerHTML = '';
+		var titleLink = linkOpenInNewWindow(feedEntry.link, feedEntry.title);
+		articleTitle.appendChild(titleLink);
 
-        li.onclick = function() {
-            showArticle(feedEntry, that);
-        };
-    }
-
-    function showArticle(feedEntry, that) {
-        var articleTitle = $("articleTitle");
-        articleTitle.innerHTML = '';
-        var titleLink = linkOpenInNewWindow(feedEntry.link, feedEntry.title);
-        articleTitle.appendChild(titleLink);
-
-        var contentBlock = $("articleContent");
-        contentBlock.innerHTML = feedEntry.content;
-        var showArticlePage = $("showArticlePage");
-        showArticlePage.innerHTML = "";
-        showArticlePage.appendChild(articleTitle);
-        showArticlePage.appendChild(contentBlock);
-        that.UI.pagestack.push('showArticlePage',
-                {subtitle: 'show Article'});
-    }
+		var contentBlock = $("articleContent");
+		contentBlock.innerHTML = feedEntry.content;
+		var showArticlePage = $("showArticlePage");
+		showArticlePage.innerHTML = "";
+		showArticlePage.appendChild(articleTitle);
+		showArticlePage.appendChild(contentBlock);
+		that.UI.pagestack.push('showArticlePage', {
+			subtitle : 'show Article'
+		});
+	}
 
 };
