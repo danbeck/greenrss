8bd79561dba2c083c526ed4f4624e9a3f11d5ed2
Daniel Beck <d.danielbeck@googlemail.com>
2013-08-31 07:33:30 +0200

    Refactoring theOldReader.js

--- public_html/js/TheOldReader.js
+++ public_html/js/TheOldReader.js
@@ -12,21 +12,26 @@ TheOldReader.prototype.getLoginToken = function(email, password, gotToken) {
     });
 };
 
+
+TheOldReader.prototype.getSubscriptionList = function(email, password,
+        onGetSubscriptionList) {
+    this.__retrieveTokenIfNecessary(email, password, onGetSubscriptionList);
+    var url = self.THEOLDREADER_API_URL + "subscription/list?output=json";
+    getHttpRequest(url, func);
+};
+
+
+TheOldReader.prototype.__retrieveTokenIfNecessary = function(email, password,
+        func) {
+    var self = this;
+    if (!this.token)
+        self.getLoginToken(email, password, function() {
+            self.__retrieveTokenIfNecessary(email, password, func);
+        });
+};
+
 TheOldReader.prototype.__saveToken = function(response, func) {
     var jsonResponse = JSON.parse(response);
     this.token = jsonResponse.Auth;
     func();
-};
-
-TheOldReader.prototype.getSubscriptionList = function(email, password,
-        onGetSubscriptionList) {
-    var self = this;
-    if (!this.token)
-        self.getLoginToken(email, password, function() {
-            self.getSubscriptionList(email, password, onGetSubscriptionList);
-        });
-    else {
-        var url = self.THEOLDREADER_API_URL + "subscription/list?output=json";
-        getHttpRequest(url, onGetSubscriptionList);
-    }
 };
