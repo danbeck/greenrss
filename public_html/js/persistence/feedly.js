211312908a43c404755c6196c12b079444929037
daniel <daniel@daniel-laptop.fritz.box>
2014-01-19 23:02:52 +0100

    Images from feedly are shown in the login screen.

--- public_html/js/persistence/feedly.js
+++ public_html/js/persistence/feedly.js
@@ -1,11 +1,14 @@
 function Feedly() {
 //    this.BASE_URL = "http://cloud.feedly.com/v3/";
-    this.BASE_URL = "http://sandbox.feedly.com/v3";
+    this.HOST_URL = "http://sandbox.feedly.com";
+    this.BASE_URL = this.HOST_URL + "/v3";
 
     //----------------------------------
 }
 
 Feedly.prototype.loginUser = function(success) {
+    var that = this;
+
     var url = this.BASE_URL + "/auth/auth";
     var urlParameters = {
         response_type: "code",
@@ -14,7 +17,12 @@ Feedly.prototype.loginUser = function(success) {
         scope: "https://cloud.feedly.com/subscriptions"
     };
 
-    $.get(url, urlParameters).success(success);
+    $.get(url, urlParameters).success(function(response) {
+        response = response.replace(/src="\//g, 'src="' + that.HOST_URL + '/');
+//        var matches = response.match(/<style>(.*)<\/style>/m)
+//        var style = matches[1];
+        success(response);
+    });
 };
 
 
